730e9727bb86f2b4da598eb3857cc11a0ebfba77
m-darawsheh <mohamaddarawsha2005@gmail.com>
2025-08-05 12:23:16 +0300

    error on line 53 mod00 ex01

--- module_00/ex01/test.cpp
+++ module_00/ex01/test.cpp
@@ -3,16 +3,69 @@
 class Contact
 {
 	private :
-
+		std::string  first_name;
+		std::string  last_name;
+		std::string  nickname;
+		std::string  phone_number;
+		std::string  darkest_secret;
 	public:
+	Contact()
+	{
+		first_name = "";
+		last_name = "";
+		nickname = "";
+		phone_number = "";
+		darkest_secret = "";
+	};
+	void setFirstName(const std::string& name) { first_name = name; }
+	void setLastName(const std::string& name) { last_name = name; }
+	void setNickname(const std::string& name) { nickname = name; }
+	void setPhoneNumber(const std::string& number) { phone_number = number; }
+	void setDarkestSecret(const std::string& secret) { darkest_secret = secret; }
+
+	std::string getFirstName() const { return first_name; }
+	std::string getLastName() const { return last_name; }
+	std::string getNickname() const { return nickname; }
+	std::string getPhoneNumber() const { return phone_number; }
+	std::string getDarkestSecret() const { return darkest_secret; }
 };
 class PhoneBook
 {
 	private:
-	Contact contacts[8];
+	Contact contact[8];
+	int index_of_contacts;
 	public:
-
+	PhoneBook()
+	{
+		index_of_contacts = 0;
+	}
+	void addContact();
+	void searchContact();
 };
+
+void PhoneBook ::addContact()
+{
+	if (index_of_contacts < 8)
+	{
+		while (true)
+		{
+			std::cout << "Please enter first name: ";
+			std::getline(std::cin, contact[index_of_contacts].getFirstName());
+			if (!contact[index_of_contacts].getFirstName().empty())
+				break;
+			std::cout << "First name cannot be empty. Please try again." << std::endl;
+		}
+	}
+	else
+	{
+		// here must be delete the first contact and add the new one
+	}
+}
+
+void PhoneBook ::searchContact()
+{
+	// here must be search for a contact
+}
 
 int main (void)
 {
@@ -24,29 +77,31 @@ int main (void)
 	std::string EXIT = "EXIT";
 
 	std::cout << "Welecom to phonebook 😁" << "\n";
-	std:: cout << "You have just three commands 'ADD' 'SEARCH' 'EXIT'" << "\n";
-	std:: cout << "-- ADD for add new contact --" << "\n";
-	std:: cout << "-- SEARCH for search a specific contact --" << "\n";
-	std:: cout << "-- EXIT for exit from program --" << "\n";
-	std:: cout << "please enter one of these commands ";
-	std:: cin >> user_input;
+	while (true)
+	{
+		std:: cout << "You have just three commands 'ADD' 'SEARCH' 'EXIT'" << "\n";
+		std:: cout << "-- ADD for add new contact --" << "\n";
+		std:: cout << "-- SEARCH for search a specific contact --" << "\n";
+		std:: cout << "-- EXIT for exit from program --" << "\n";
+		std:: cout << "please enter one of these commands ";
+		std:: getline(std::cin, user_input);
 
-	if (ADD.compare(user_input) == 0)
-	{
-		// ADD command
-	}
-	else if (SEARCH.compare(user_input) == 0)
-	{
-		// SEARCH command
-	}
-	else if (EXIT.compare(user_input) == 0)
-	{
-		// EXIT command
-		exit(0);
-	}
-	else
-	{
-		// please enter one of these three command ADD SEARCH EXIT
+		if (ADD.compare(user_input) == 0)
+		{
+			// ADD command
+		}
+		else if (SEARCH.compare(user_input) == 0)
+		{
+			// SEARCH command
+		}
+		else if (EXIT.compare(user_input) == 0)
+		{
+			break;
+		}
+		else
+		{
+			std::cout << "Invalid command, please try again." << "\n";
+		}
 	}
 	return 0;
 
