83ac01ca62f27275bc6890b48f84c90662890537
m-darawsheh <mohamaddarawsha2005@gmail.com>
2025-08-19 18:29:30 +0300

    mod01 finish ex01

---
+++ module_01/ex00/Zombie.hpp
@@ -0,0 +1,18 @@
+#ifndef ZOMBIE_HBB
+ #define ZOMBIE_HBB
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	private:
+	std::string name;
+	public:
+	void announce( void );
+	Zombie(std::string name );
+	~Zombie();
+};
+Zombie* newZombie( std::string name );
+void randomChump( std::string name );
+#endif
