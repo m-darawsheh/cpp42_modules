a46808cf19d6bd2b199b63141e7a87e8a663b398
m-darawsheh <mohamaddarawsha2005@gmail.com>
2025-08-20 10:33:01 +0300

    mod01 ex01 is done

---
+++ module_01/ex01/Zombie.hpp
@@ -0,0 +1,26 @@
+#ifndef ZOMBIE_HBB
+ #define ZOMBIE_HBB
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	private:
+	std::string name;
+	public:
+	void announce( void );
+	Zombie();
+	void setName(std::string name)
+	{
+		this->name = name;
+	}
+	~Zombie();
+};
+
+
+
+
+Zombie* zombieHorde( int N, std::string name );
+
+#endif
---
+++ module_01/ex01/main.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+int main ()
+{
+	int N = 5;
+	std::string name = "Walker";
+	Zombie* horde = zombieHorde(N, name);
+
+	for (int i = 0; i < N; i++)
+	{
+		horde[i].announce();
+	}
+
+	delete[] horde;
+	return 0;
+}
