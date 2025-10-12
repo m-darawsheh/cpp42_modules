c5fdade5a7340cd028d25b84f669a1b52860ade9
Mohammed Darawsheh <mdarawsh@c2r2s4.42amman.com>
2025-10-12 17:29:46 +0300

    l

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -22,7 +22,51 @@ RPN &RPN::operator=(const RPN &obj)
     return *this;
 }
 
+bool is_valid_token(const std::string &token)
+{
+    if (token.length() == 1 && (isdigit(token[0]) || token == "+" || token == "-" || token == "*" || token == "/"))
+    {
+        return true;
+    }
+    return false;
+}
+
+// top
+// size
+// empty
+// push
+// pop
+// swap
+void RPN::checks()
+{
+
+}
+
+bool is_operator(std::string token)
+{
+    if (token == "+" ||  token == "-" || token == "*" || token == "/")
+        return true;
+    return false ;
+}
+
+void RPN::calculate_expression(std::string &expression)
+{
+    std::istringstream my_expr(expression);
+    std::string token;
+    while(getline(my_expr, token, ' '))
+    {
+        if(!is_valid_token(token))
+        {
+            return;
+        }
+        // if(is_operator(token) && this->stack.size() == )
+
+    }
+}
+
+
 RPN::RPN(std::string expression)
 {
+    calculate_expression(expression);
 
 }
--- module_09/ex01/RPN.hpp
+++ module_09/ex01/RPN.hpp
@@ -4,16 +4,21 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <stack>
+#include <sstream>
 
 class RPN {
     private:
-    
+    std::stack<int> stack;
     public:
     RPN();
     ~RPN();
     RPN(const RPN &obj);
     RPN(std::string expression);
     RPN &operator=(const RPN &obj);
+    void calculate_expression(std::string &expression);
+    void RPN::checks();
+
 };
 
 #endif
